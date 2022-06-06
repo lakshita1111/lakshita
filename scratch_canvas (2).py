669ea4457f2f5246f1303dd3b81a654b133e7247
lakshita1111 <104756373+lakshita1111@users.noreply.github.com>
2022-06-06 09:08:46 +0530

    Add files via upload

---
+++ scratch_canvas (2).py
@@ -0,0 +1,13 @@
+import turtle
+
+for i in range(2):
+    turtle.shape("square")#Write the shape as square
+    turtle.pencolor("red")#Write the pen color as red
+    turtle.forward(200)
+    turtle.left(90)
+    
+for i in range(2):
+    turtle.shape("circle")#Write the shape as circle
+    turtle.pencolor("green")#Write the pen color as green
+    turtle.forward(200)
+    turtle.left(90)
